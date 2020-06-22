cfd2b0a857691e36041e31b7c91a7827e290ca9e
David Juarez <djuar05@gmail.com>
2020-06-22 11:24:04 -0400

    added the html for results

--- src/main/java/com/win/subscriberlist/subscriber/SubscriberController.java
+++ src/main/java/com/win/subscriberlist/subscriber/SubscriberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SubscriberController {
@@ -20,6 +21,7 @@ public class SubscriberController {
     // takes the data entered in the form and adds it to the database
     // Posts the data then displays a new template called "result"
     private Subscriber subscriber;
+    @PostMapping
     public String addNewSubscriber(Subscriber subscriber, Model model){
         subscriberRepository.save(new Subscriber(subscriber.getFirstName(), subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
         model.addAttribute("firstName", subscriber.getFirstName());
